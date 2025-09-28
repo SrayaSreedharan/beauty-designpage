b6a444f77c32c93e6c3f3479ecb119b9f36d5ba8
SRAYA SREEDHARAN <srayasreedharan111@gmail.com>
2025-09-28 11:22:27 +0530

    add signature section

--- src/component/signature/SignatureServices.jsx
+++ src/component/signature/SignatureServices.jsx
@@ -5,19 +5,13 @@ const SignatureServices = () => {
   return (
     <div className="containers">
     <div className="signature-section">
-      {/* Top full-width image */}
       <div className="top-image">
         <img src="/images/hh.jpeg" alt="Lashes" />
       </div>
-
-      {/* Bottom content */}
       <div className="bottom-section">
-        {/* Left image */}
         <div className="service-image">
         <img src="/images/hh.jpeg" alt="Lashes" />
         </div>
-
-        {/* Right text */}
         <div className="service-text">
           <h2>SIGNATURE <br /> SERVICES</h2>
           <p>
